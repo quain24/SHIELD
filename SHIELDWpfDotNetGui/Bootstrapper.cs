cb234ddef903451cd438f71064db6dee33cee231
Henryk Wiśniewski <henryk.wisniewski@gmail.com>
2019-08-18 15:23:00 +0200

    Simple container config
    Configured Auto dependency injection with simple container, for now for viewModels and some singletones like WindowManager and EventAggregator

--- SHIELDWpfDotNetGui/Bootstrapper.cs
+++ SHIELDWpfDotNetGui/Bootstrapper.cs
@@ -11,14 +11,52 @@ namespace SHIELDGui
 {
     public class Bootstrapper : BootstrapperBase
     {
+        // konf simple container, continued on bottom 
+        private SimpleContainer _container = new SimpleContainer();
+        
+        // Initialize - requires additional config in app.xaml (resourceDictionary... etc bootstrapper)
         public Bootstrapper()
         {
             Initialize();
         }
 
+        // Start this window after boot
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
         }
+
+
+        #region Simple Container configuration 
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType =>
+                         _container.RegisterPerRequest(viewModelType, viewModelType.ToString(), viewModelType));
+        }
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        } 
+        #endregion
     }
 }
