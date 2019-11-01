6d882fb1e0ad299279b907350b89f059e41257ce
Henryk Wiśniewski <henryk.wisniewski@gmail.com>
2019-11-01 17:43:14 +0100

    ComCommander optimization and small fixes
    Switched MessageError enum to separate file,
    started working on events - custom EA classes and declarations. Moved some checking fro message handlers to Conductor method - only confirmation handler still switches buffers outside of Conductor method. Added Timeout for sent messages and confirmations - for single messages and whole buffers.
    Additional functionality in Timestamp class - differences etc.

---
+++ Shield/HardwareCom/CommandEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using Shield.HardwareCom.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shield.HardwareCom
+{
+    public class CommandEventArgs : EventArgs
+    {
+        private ICommandModel _command;
+
+        public CommandEventArgs(ICommandModel command)
+        {
+            _command = command;
+        }
+
+        public ICommandModel Message { get { return _command; } }
+    }
+}
