e98ad930884a82f6b6a9e08738c44132e43d500d
Henryk Wiśniewski <henryk.wisniewski@gmail.com>
2020-03-28 19:32:17 +0100

    Minor component registration change in Wpf.Gui

--- Shield.WpfGui/WpfGuiAfModule.cs
+++ Shield.WpfGui/WpfGuiAfModule.cs
@@ -1,13 +1,8 @@
 ﻿using Autofac;
 using Caliburn.Micro;
+using Shield.WpfGui.ViewModels;
 using System.Linq;
 using System.Reflection;
-using Shield.WpfGui.ViewModels;
-using Autofac.Core;
-using Shield.HardwareCom.Factories;
-using Shield.HardwareCom.MessageProcessing;
-using System;
-using Shield.HardwareCom.Models;
 
 namespace Shield.WpfGui
 {
@@ -23,44 +18,17 @@ namespace Shield.WpfGui
                 .As<IEventAggregator>()
                 .SingleInstance();
 
-            builder.RegisterAssemblyTypes(Assembly.Load($"{nameof(Shield)}.{nameof(WpfGui)}"))
-                .Where(t => t.IsInNamespace("Shield.WpfGui.ViewModels") && t.Name.EndsWith("ViewModel"))
-                .Except<ShellViewModel>()
+
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .Where(t => t.IsInNamespace($@"Shield.WpfGui.ViewModels") && t.Name.EndsWith("ViewModel"))
                 .AsSelf();
 
-            builder.RegisterAssemblyTypes(Assembly.Load($"{nameof(Shield)}.{nameof(WpfGui)}"))
-                .Where(t => t.IsInNamespace("Shield.WpfGui.Models") && t.Name.EndsWith("Model"))
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .Where(t => t.IsInNamespace($@"Shield.WpfGui.Models") && t.Name.EndsWith("Model"))
                 .AsSelf();
 
-            builder.RegisterType<ShellViewModel>()                   
-                   //.WithParameters(new[]
-                   //{
-                   //    new ResolvedParameter(
-                   //        (pi, ctx) => pi.Name == "messanger",
-                   //        (pi, ctx) => ctx.Resolve<HardwareCom.IMessanger>()),
-                   //    new ResolvedParameter(
-                   //        (pi, ctx) => pi.Name == "settings",
-                   //        (pi, ctx) => ctx.Resolve<Data.ISettings>()),
-                   //    new ResolvedParameter(
-                   //        (pi, ctx) => pi.Name == "messageFactory",
-                   //        (pi, ctx) => ctx.Resolve<Func<IMessageModel>>()),
-                   //    new ResolvedParameter(
-                   //        (pi, ctx) => pi.Name == "commandFactory",
-                   //        (pi, ctx) => ctx.Resolve<HardwareCom.Factories.ICommandModelFactory>()),
-                   //    new ResolvedParameter(
-                   //        (pi, ctx) => pi.Name == "commandIngester",
-                   //        (pi, ctx) => ctx.Resolve<HardwareCom.ICommandIngester>()),
-                   //    new ResolvedParameter(
-                   //        (pi, ctx) => pi.Name == "incomingMessageProcessor",
-                   //        (pi, ctx) => ctx.Resolve<HardwareCom.IIncomingMessageProcessor>()),
-                   //    new ResolvedParameter(
-                   //        (pi, ctx) => pi.Name == "confirmationFactory",
-                   //        (pi, ctx) => ctx.Resolve<IConfirmationFactory>()),
-                   //    new ResolvedParameter(
-                   //        (pi, ctx) => pi.Name == "confirmationTimeoutChecker",
-                   //        (pi, ctx) => ctx.Resolve<IConfirmationTimeoutChecker>())
-                   //})
-                   .AsSelf();
+            //builder.RegisterType<ShellViewModel>()
+            //       .AsSelf();
 
             base.Load(builder);
         }
