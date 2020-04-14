6f67446ec0462b6443dde8d784c2ccbbcf524f78
Henryk Wiśniewski <henryk.wisniewski@gmail.com>
2020-04-14 21:13:36 +0200

    Began work on new IncomingDataPreparer
    Refactoring of old IncomingDataPreparer for cleaner code.
    Unit testing of newer version.

---
+++ Shield/HardwareCom/CommandProcessing/CommandTranslatorNew.cs
@@ -0,0 +1,103 @@
+﻿using Shield.Data.Models;
+using Shield.HardwareCom.Enums;
+using Shield.HardwareCom.Models;
+using Shield.Helpers;
+using System;
+using System.Text;
+
+namespace Shield.HardwareCom.CommandProcessing
+{
+    /// <summary>
+    /// Translates given CommandModel into 'string' of raw data that could be sent and vice versa.
+    /// </summary>
+    public class CommandTranslatorNew : ICommandTranslator
+    {
+        private readonly int _commandLengthWithData;
+        private readonly int _commandLength;
+        private readonly char _separator;
+        private readonly char _filler;
+
+        private readonly Func<ICommandModel> _commandModelFac;
+        private readonly IApplicationSettingsModel _appSettingsModel;
+
+        public CommandTranslatorNew(IApplicationSettingsModel applicationSettings, Func<ICommandModel> commandModelFac)
+        {
+            _commandModelFac = commandModelFac ?? throw new ArgumentNullException(nameof(commandModelFac)); // Autofac factory
+            _appSettingsModel = applicationSettings ?? throw new ArgumentNullException(nameof(applicationSettings));
+
+            _separator = _appSettingsModel.Separator;
+            _filler = _appSettingsModel.Filler;
+            _commandLength = _appSettingsModel.CommandTypeSize + _appSettingsModel.IdSize + 3;
+            _commandLengthWithData = _commandLength + _appSettingsModel.DataSize + 1;
+        }
+
+        public ICommandModel FromString(string rawData)
+        {
+            ICommandModel command = _commandModelFac();
+            string rawCommandTypeString = string.Empty;
+            string rawDataString = string.Empty;
+            string rawIdString = string.Empty;
+
+            if (rawData.Length == _commandLengthWithData || rawData.Length == _commandLength)
+            {
+                rawCommandTypeString = rawData.Substring(1, _appSettingsModel.CommandTypeSize);
+                rawIdString = rawData.Substring(2 + _appSettingsModel.CommandTypeSize, _appSettingsModel.IdSize);
+
+                if (rawData.Length == _commandLengthWithData)
+                    rawDataString = rawData.Substring(_commandLength);
+
+                int rawComInt;
+                if (int.TryParse(rawCommandTypeString, out rawComInt))
+                {
+                    if (Enum.IsDefined(typeof(CommandType), rawComInt))
+                        command.CommandType = (CommandType)rawComInt;
+                    else
+                        command.CommandType = CommandType.Unknown;
+
+                    command.Id = rawIdString;
+                    command.Data = rawData.Length == _commandLengthWithData ? rawDataString : string.Empty;
+                }
+                else
+                {
+                    command.CommandType = CommandType.Error;
+                    command.Id = string.Empty.PadLeft(_appSettingsModel.IdSize, _filler);
+                    command.Data = rawData.Length > _appSettingsModel.DataSize ? rawData.Substring(0, _appSettingsModel.DataSize) : rawData;
+                }
+            }
+            else
+            {
+                command.CommandType = CommandType.Error;
+                command.Id = string.Empty.PadLeft(_appSettingsModel.IdSize, _filler);
+                command.Data = rawData.Length > _appSettingsModel.DataSize ? rawData.Substring(0, _appSettingsModel.DataSize) : rawData;
+            }
+
+            command.TimeStamp = Timestamp.TimestampNow;
+            return command;
+        }
+
+        /// <summary>
+        /// Translates a CommandModel into a raw formatted string if given a correct command or returns empty string for error
+        /// </summary>
+        /// <param name="givenCommand">Command to be transformed into raw string</param>
+        /// <returns>Raw formatted string that can be understood by connected machine</returns>
+        public string FromCommand(ICommandModel givenCommand)
+        {
+            if (givenCommand is null || !Enum.IsDefined(typeof(CommandType), givenCommand.CommandType))
+                return null;
+
+            StringBuilder command = new StringBuilder(_separator.ToString());
+
+            command.Append(((int)givenCommand.CommandType).ToString().ToUpperInvariant().PadLeft(_appSettingsModel.CommandTypeSize, '0')).Append(_separator);
+            command.Append(givenCommand.Id).Append(_separator);
+
+            if (givenCommand.CommandType == CommandType.Data)
+            {
+                command.Append(givenCommand.Data);
+                command.Append(_filler, _commandLengthWithData - 1 - command.Length);
+                command.Append(_separator);
+            }
+
+            return command.ToString();
+        }
+    }
+}
---
+++ ShieldTests/HardwareCom/RawDataProcessing/IncomingDataPreparerTests.cs
@@ -0,0 +1,116 @@
+﻿using Shield.HardwareCom.RawDataProcessing;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Xunit;
+
+namespace ShieldTests.HardwareCom.RawDataProcessing
+{
+    public class IncomingDataPreparerTests
+    {
+        public IncomingDataPreparer2 TestPreparer;
+        public string goodData => "*0001*0001*0123456789";
+        public string badDataPack = "*0002*0002*012345*789";
+        public string badDataPackShort = "*0003*0003*012345*7";
+        public string badDataShort = "*0004*0004*01234567";
+        public string good = "*0005*0005*";
+        public string badId = "*0006*.$06*";
+        public string badShort = "*0007*12";
+        public string badMissingFrontSep = "0008*0008*";
+
+        public IncomingDataPreparerTests()
+        {
+            TestPreparer = new IncomingDataPreparer2(4, 4, 10,
+                                                     new Regex("[*][0-9]{4}[*][a-zA-Z0-9]{4}[*]"),
+                                                     '*');
+        }
+
+        [Theory]
+        [InlineData("*0001*0000")]
+        [InlineData("*0001*000")]
+        [InlineData("*0001*")]
+        [InlineData("*00")]
+        public void DataSearch_will_returns_empty_list_when_given_too_short_data(string data)
+        {
+            var output = TestPreparer.DataSearch(data);
+            Assert.Equal(new List<string>(), output);
+        }
+
+        [Theory]
+        [InlineData("*0018*0001*0123456789")]
+        [InlineData("*0018*0001*0123456789", "*0018*0002*1231231231")]
+        [InlineData("*0018*0001*0123456789", "*0018*0002*1231231231", "*0018*0003*1231231231")]
+        [InlineData("*0001*0001*", "*0018*0002*1231231231")]
+        public void DataSearh_will_return_list_of_commands_given_good_data(params string[] data)
+        {
+            var actual = new List<string>();
+            var expected = new List<string>();
+
+            foreach (var s in data)
+            {
+                actual.AddRange(TestPreparer.DataSearch(s));
+                expected.Add(s);
+            }
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("*0018*0001*012345678901234567890123456789", "*0018*0001*0123456789", "01234567890123456789")]
+        [InlineData("*0018*0001*0123456789*0018*0002*012345678901234567890123456789", "*0018*0001*0123456789", "*0018*0002*0123456789", "01234567890123456789")]
+        [InlineData("123456789*0018*0001*0123456789*0018*0002*0123456789", "123456789", "*0018*0001*0123456789", "*0018*0002*0123456789")]
+        [InlineData("*0018*0001*0123456789123456789*0018*0002*0123456789", "*0018*0001*0123456789", "123456789", "*0018*0002*0123456789")]
+        public void DataSearh_should_return_good_command_and_trash_given_good_data_and_bad_data_with_no_separator_in_bad_data(string data, params string[] expectedData)
+        {
+            var actual = TestPreparer.DataSearch(data);
+            var expected = new List<string>();
+
+            expected.AddRange(expectedData);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("0000000000000")]
+        [InlineData("123123123123")]
+        public void DataSearch_should_return_bad_data_if_given_bad_data_longer_than_command_length_with_no_separator(string data)
+        {
+            var actual = TestPreparer.DataSearch(data);
+            var expected = new List<string>() { data };
+
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Theory]
+        [InlineData("*0018*0001*01234567",
+                    "*0018*0002*0123456789")]
+        [InlineData("*0018*0001*01234567",
+                    "*0018*0002*0123456789",
+                    "*0018*0001*01234567",
+                    "*0018*0002*0123456789")]
+        [InlineData("*0018*0001*01234567",
+                    "*0018*0001*01234567",
+                    "*0018*0001*01234567",
+                    "*0018*0002*0123456789",
+                    "*0018*0001*01234567",
+                    "*0018*0002*0123456789")]
+        [InlineData("*0018*0001*01234567",
+                    "*0018*0001*",
+                    "*0018*0001*01234567",
+                    "*0018*0002*0123456789",
+                    "*0018*0001*01234567",
+                    "*0003*0001*",
+                    "*0018*0002*0123456789",
+                    "*0001*0001*")]
+        public void DataSearch_returns_junk_and_data_given_too_short_data_command_and_normal_data_command_no_junk_at_end(params string[] expectedData)
+        {
+            var data = string.Concat(expectedData);
+            var actual = TestPreparer.DataSearch(data);
+            var expected = new List<string>();
+
+            expected.AddRange(expectedData);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
