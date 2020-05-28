cb8899a61e24319142a30ec3ec3c7d09bd0c7d3f
Henryk Wiśniewski <henryk.wisniewski@gmail.com>
2020-05-28 21:50:18 +0200

    Removed unused files

--- Shield.Messaging/Commands/Parts/CommandPartValidators/IValidatorAssingnmentFactory.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Shield.Messaging.Commands.Parts.CommandPartValidators
-{
-    public interface IValidatorAssingnmentFactory
-    {
-        IReadOnlyPartValidatorCollection GetValidatorAssignments();
-    }
-}
