b746268301452260d6f1528949983081b39638f1
Henryk Wiśniewski <henryk.wisniewski@gmail.com>
2020-09-30 21:21:56 +0200

    Added json datapack for future device creation. Added System.text.json for json serialization

---
+++ Shield.Messaging/Protocol/DataPacks/JsonDataPack.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+
+namespace Shield.Messaging.Protocol.DataPacks
+{
+    public class JsonDataPack : IDataPack
+    {
+        private readonly string _serializedObject;
+
+        public JsonDataPack(object toBeSerialized)
+        {
+            _serializedObject = Serialize(toBeSerialized);
+        }
+
+        private string Serialize(object toBeSerialized)
+        {
+            return JsonSerializer.Serialize(toBeSerialized, new JsonSerializerOptions { PropertyNameCaseInsensitive = false });
+        }
+
+        public string GetDataInTransmittableFormat()
+        {
+            return _serializedObject;
+        }
+    }
+}
